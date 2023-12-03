32e05197cca3d48b1a29a0aae0a9940c4b54c3b1
Arinjoy Ghosh <91753320+ArinjoyGhosh@users.noreply.github.com>
2023-12-03 12:52:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+srk = 'Darr Kiran.mp3';
+bshah = 'shehar ki ladki.mp3';
+lx = 0;
+ly = 0;
+rx = 0;
+ry = 0;
+sl = 0;
+statusSong1 = "";
+scoreLeft = 0;
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.position(425, 210);
+
+    video = createCapture(VIDEO);
+    video.position(canvas.x, canvas.y);
+
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function preload() {
+    loadSound(srk);
+    loadSound(bshah);
+}
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log('Got Results');
+        console.log(results);
+
+        lx = results[0].pose.leftWrist.x;
+        ly = results[0].pose.leftWrist.y;
+        rx = results[0].pose.rightWrist.x;
+        ry = results[0].pose.rightWrist.y;
+
+        scoreLeft = results[0].pose.keypoints[9].score;
+        console.error(scoreLeft);
+
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+    if (scoreLeft > 0.000002) {
+        fill("#EE1C03");
+        circle(lx, ly, 50);
+        srk.play();
+        document.getElementById('h2').innerHTML = 'Jaadu Teri Nazar';
+    }
+}
